384f985e4c7381da41ba4b9c2cfa8902a9c1e628
Ueveton <uevetonsp@gmail.com>
2024-10-19 14:21:07 -0300

    CSS inputs

--- src/Components/ImcFront.jsx
+++ src/Components/ImcFront.jsx
@@ -3,9 +3,36 @@ import './ImcFront.css'
 
 export const ImcFront = () => {
     return (
-        <div className='princ'>
-            <h2>Calculador de IMC</h2>
-            <button>te</button>
+        <div id='front-container'>
+            <h2>Calculadora de IMC</h2>
+            <form id='imc-form'>
+                <div className='form-inputs'>
+                    <div className='form-control'>
+                        <label htmlFor="height">Altura:
+                            <input 
+                                type="text" 
+                                name='height'
+                                id='height' 
+                                placeholder='Exemplo 1,75'
+                            />
+                        </label>
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="weight">Peso:
+                            <input 
+                                type="text" 
+                                name='weight'
+                                id='weight' 
+                                placeholder='Exemplo 80,5'
+                            />
+                        </label>
+                    </div>
+                </div>
+                <div className="action-control">
+                    <button>Calcular</button>
+                    <button>Limpar</button>
+                </div>
+            </form>
         </div>
     )
 }
